cca2b045d8fae585641f2b81d99a389b1d9f33b5
JakeRoughton <112968037+JakeRoughton@users.noreply.github.com>
2022-10-27 14:44:12 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,213 +1,68 @@
-const prompt = require('prompt-sync')({sigint: true});
-
-console.log('Welcome to the To-Do List Manager Application!\n')
-
-let option = selectOption();
+const prompt = require('prompt-sync')({ sigint: true });
+console.log("Welcome to Aaron's To-Do List Application!\n");
+console.log("~ Select An Action ~");
+console.log("[1] Create A To-Do Item");
+console.log("[2] Complete A To-Do Item");
+console.log("[3] Exit To-Do List Application");
+let option = Number(prompt('> '));
 let toDoList = [];
 let statusArray = [];
-
-while(option !== 5){
-    if(option === 1){
+while (option !== 3) {
+    if (option === 1) {
         console.log('\n~ Creating a new to-do item ~');
-        console.log('What is this to-do item called?');
-    let addItem = prompt('> ')
-    while(addItem.length === 0){
-        console.log('Invalid: Input cannot be empty')
-        addItem = prompt('> ');
-}
-    toDoList.push(addItem);
-    statusArray.push(false);
-
-    displayList();
-
-    option = selectOption();
-
-
-
-}   else if (option === 2){
-        if(toDoList.length !== 0){
-        console.log('~ Completing a to-do item ~');
-        console.log('Which to-do item would you like to complete?');
-
+        console.log('What is this to-do item called? ');
+        let addItem = prompt('> ');
+        while (addItem.length === 0) {
+            console.log("Invalid Input");
+            addItem = prompt(' > ');
+        }
+        toDoList.push(addItem)
+        statusArray.push(false);
         displayList();
-
-        let newStatus = Number(prompt('> '));
-
-
-        while(isNaN(newStatus) || newStatus > statusArray.length || newStatus < 1){
-            console.log('Please input a number that corresponds with item in list: ')
-            newStatus = Number(prompt('> '));
-}
-        statusArray[newStatus-1]= true;
-}       else{
-        console.log("\n***Please add task before completing item***\n")
+        selectOption();
+    } else if (option === 2) {
+        if (toDoList.length !== 0) {
+            console.log('~ Completing a to-do item ~');
+            console.log('Which to-do item would you like to complete? ');
             displayList();
-
-        option = selectOption();
-}
-
-}   else  {
-        console.log('Error!!! Invalid operation')
-        option = selectOption();
-
-}   if (option === 3){
-    if(toDoList.length !== 0){
-    console.log('~ Unchecking a to-do item ~');
-    console.log('Which to-do item would you like to uncheck?');
-    
-    displayList();
-
-    newStatus = Number(prompt('> '));
-
-    while(isNaN(newStatus) || newStatus > statusArray.length || newStatus < 1){
-        console.log('Please input a number that corresponds with item in list: ')
-        newStatus = Number(prompt('> '));
-    }
-    statusArray[newStatus-1]= false;
-}   else{
-    console.log("\n***Please add task before unchecking item***\n")
+            let newStatus = Number(prompt('> '))
+            while (isNaN(newStatus) || newStatus > statusArray.length || newStatus < 1) {
+                console.log('Please input a number from the given options');
+                newStatus = Number(prompt('> '))
+            }
+            statusArray[newStatus - 1] = true;
+        } else {
+            console.log("please add something to your to-do list before trying to comeplete an item. ");
+        }
         displayList();
-
-    option = selectOption();
+        selectOption();
+    } else {
+        console.log("\nInvalid operation");
+        selectOption()
+    }
 }
-    displayList();
-
-    option = selectOption();
-
-    displayList();
-
-} else {
-    console.log('Error!!! Invalid operation')
-    option = selectOption();
+// Exiting Application
+console.log("~ Exiting the Application ~ ");
+function selectOption() {
+    console.log("\n~ Select An Action ~");
+    console.log("[1] Create A To-Do Item");
+    console.log("[2] Complete A To-Do Item");
+    console.log("[3] Exit To-Do List Application");
+    option = Number(prompt(' > '))
 }
-
-// }   if (option === 4){
-//     if(toDoList.length !== 0){
-//     console.log('~ Deleting a to-do item ~');
-//     console.log('Which to-do item would you like to Delete?');
-
-//     displayList();
-
-//     newStatus = Number(prompt('> '));
-
-
-//     while(isNaN(newStatus) || newStatus > statusArray.length || newStatus < 1){
-//         console.log('Please input a number that corresponds with item in list: ')
-//         newStatus = Number(prompt('> '));
-// }
-//     statusArray[newStatus.splice(Number(prompt))];
-// }   else{
-//     console.log("\n***Please add task before deleting item***\n")
-//         displayList();
-
-//     option = selectOption();
-// }
-
-// }   else  {
-//     console.log('Error!!! Invalid operation')
-//     option = selectOption();
-
-//Exiting Application
-}
-
-function selectOption(){
-console.log('\n~ Select an action ~');
-console.log('[1] Create a to-do item');
-console.log('[2] Complete a to-do item');
-console.log('[3] Uncheck to-do item');
-console.log('[4] Delete to-do item')
-console.log('[5] Exit To-Do-List');
-return Number(prompt('> '));
-}
-
-function displayList(){
-
-    if(toDoList.length === 0){
-        console.log("Your To-Do list is empty");
-    }   else {
-        console.log(`You have ${toDoList.length} to do item(s) `)
-
-}
-    for(let i = 0; i < toDoList.length; i++){
-        let status = "";
-
-          if(statusArray[i] === false){
-            status = "[incomplete]";
-        } else if(statusArray[i] === true){
-            status = "[complete]";
+function displayList() {
+    if (toDoList.length === 0) {
+        console.log("Your List Is Empty");
+    } else {
+        console.log(`you have ${toDoList.length} to-do item(s)`);
+    }
+    let status = " ";
+    for (let i = 0; i < toDoList.length; i++) {
+        if (statusArray[i] === false) {
+            status = "[Incomplete]";
+        } else if (statusArray[i] === true) {
+            status = '[Completed]';
         }
-
-
-        console.log(`${i+1}. ${status} ${toDoList[i]}`)
-        
-
-        
-        }
-
+        console.log(`${i + 1}. ${status} ${toDoList[i]} `);
+    }
 }
-
-
-/*
-1. Figure out the UI
-console.log()
-    -Welcome message
-    -Different options (add task, complete task, exit program)
-    -Error messages for invalid options
-Prompts
-    - Numbers to decide which options using if statements
-    1 Adding task/create item - prompt user which item to complete
-    2 Mark task as complete - prompt user for which task is complete
-    3 Exit
-
-While loop    
-    -Program has no known ending so we want to be able to prompt them indefinitely until they decide to exit
-
-Display list to user
-    -Completion of status (complete or incomplete)
-    -The name of the item
-    -The number associated with the item
-    -Loop to display each item in the To-Do-List
-
-2. Add To-Do-Items (Choice === 1)
--Prompt for the to-do-item
--Save the item by storing it in an array ---with .push()
--an array to keep track of To-Do items
-
-
-2.5 - How do we set items to be incomplete
-//Any item in the list is [incomplete] until we mark it [complete]
-
-- Whenever we add an item to the ToDo List it's [incomplete] by default
-
-let completeStatus = [incomplete]
-array of booleans
-[true, false, true]                   Status Array
-['Walk Dog', 'Get Food', 'Run Mile']  toDoList Array
-  0              1           2        Shared index
-
-let status = [];
-by default, when we add a new item, we want to add a false boolean to our status array.
-
-
-
-
-
-
-
-
-
-3. Complete To-Do items (Choice === 2)
-- Display current items in ToDo list with their status
-
-- Pick which ToDo item to mark as complete - swap its status from false to true
-
-- Make sure we display the updated list right after
-
-
-
-
-
-
-
-
-*/
